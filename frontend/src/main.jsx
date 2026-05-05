5d7ef7ca230caa6c32923427c02b7ddb5478e30d
GOWTHAM SAI CHIRIKI <gowthamsai167@gmail.com>
2026-05-05 15:14:54 +0530

    Did a few up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./styles/layout.css";
+import "./styles/Layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
